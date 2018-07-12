154c0083c39b3897c212bdf21671041045a2c5c1
itsjzt <saurabh.19@outlook.com>
2018-07-12 14:07:37 +0530

    added views

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 require("dotenv").config();
+const createError = require("http-errors");
 const express = require("express");
 const path = require("path");
-const cors = require("cors");
 const bodyParser = require("body-parser");
-// // const cookieParser = require("cookie-parser");
+const cookieParser = require("cookie-parser");
 const passport = require("passport");
 const logger = require("morgan");
 const mongoose = require("mongoose");
@@ -21,11 +21,13 @@ mongoose
   );
 
 const app = express();
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 app.use(logger("dev"));
-app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(cookieParser());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
 
 const auth = require("./handlers/authHandler");
 auth(passport);
@@ -34,4 +36,21 @@ app.use(passport.initialize());
 app.use("/", authRouter);
 app.use("/users", userRouter);
 app.use("/post", postRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
 module.exports = app;
