6fad6e023b6239ee5e14110f39a2b166eabb0338
itsjzt <saurabh.19@outlook.com>
2018-07-14 12:38:22 +0530

    logout function now working

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -8,11 +8,15 @@ const {
 } = require("../controllers/userController");
 const { catchErrors } = require("../handlers/errorHandler");
 
-// All routes are relative to /user/
+// All routes are relative to /users/
 
 router.get("/follow/:me/:tofollow", catchErrors(followUser));
 router.get("/unfollow/:me/:tofollow", catchErrors(unfollowUser));
 router.get("/editusername/:oldname/:newname", catchErrors(editUsername));
 router.get("/i/:username", catchErrors(findUserByUsername));
+router.get("/logout", function(req, res) {
+  req.logout();
+  res.redirect("/");
+});
 
 module.exports = router;
