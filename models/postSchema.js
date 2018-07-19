ee511986870b434159286c8425d5560b1bc8d258
SAURABH SHARMA <saurabh.19@outlook.com>
2018-07-19 17:28:42 +0530

    + post schema

--- models/postSchema.js
+++ models/postSchema.js
@@ -3,10 +3,22 @@ const Schema = mongoose.Schema;
 
 // todo: fields required to be true
 const PostSchema = new Schema({
-  title: { type: String, trim: true },
-  url: { type: String, lowercase: true, trim: true, unique: true },
-  author: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  article: String,
+  title: { type: String, trim: true, required: true },
+  url: {
+    type: String,
+    lowercase: true,
+    trim: true,
+    unique: true,
+    required: true
+  },
+  author: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  article: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  // used when post are already a comment
+  parentPost: { type: mongoose.Schema.Types.ObjectId, ref: "Post" },
   comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Post" }],
   date: { type: Date, default: Date.now },
   meta: {
