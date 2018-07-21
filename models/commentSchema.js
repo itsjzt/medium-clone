5a101c3dfbb9bb856c627cafc7bd03370ae64205
SAURABH SHARMA <saurabh.19@outlook.com>
2018-07-21 10:19:16 +0530

    added comment schema

---
+++ models/commentSchema.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const CommentSchema = new Schema({
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  text: {
+    type: String,
+    required: "You must pass a text review",
+    trim: true
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: "You must pass a user"
+  },
+  post: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Post",
+    required: "You must pass a post"
+  }
+});
+
+module.exports = mongoose.model("Comment", CommentSchema);
