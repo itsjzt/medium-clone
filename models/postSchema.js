c35dcd344d377ed2b6b971f41082c30a080d6ba0
SAURABH SHARMA <saurabh.19@outlook.com>
2018-07-21 10:20:26 +0530

    modified post schema

--- models/postSchema.js
+++ models/postSchema.js
@@ -1,29 +1,24 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-// todo: fields required to be true
 const PostSchema = new Schema({
-  title: { type: String, trim: true, required: true },
+  title: { type: String, trim: true, required: "You must pass a title" },
   url: {
     type: String,
     lowercase: true,
     trim: true,
     unique: true,
-    required: true
+    required: "There should be a url"
   },
   author: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   article: {
     type: String,
-    required: true,
+    required: "You should have some article text",
     trim: true
   },
-  // used when post are already a comment
-  parentPost: { type: mongoose.Schema.Types.ObjectId, ref: "Post" },
-  comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Post" }],
   date: { type: Date, default: Date.now },
   meta: {
-    claps: { type: Number, default: 0 },
-    clapped_by: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }]
+    claps: { type: Number, default: 0 }
   }
 });
 
